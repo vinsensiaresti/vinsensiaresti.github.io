8f147f45e943e5493f8d9264005103080acbb6cd
= <=>
2019-11-19 09:42:38 +0700

    ugd pwa

---
+++ precache-manifest.eab7fbb33ef00ca0807e989dbf7c0a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebda24a7abd81af1880",
+    "url": "/css/chunk-vendors.c893da24.css"
+  },
+  {
+    "revision": "d7f2990a7dd238bcc5faae8a4e07a358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "088515212f4622dc22e5",
+    "url": "/js/app.217c665e.js"
+  },
+  {
+    "revision": "5ebda24a7abd81af1880",
+    "url": "/js/chunk-vendors.2848a4da.js"
+  },
+  {
+    "revision": "e32ec49dc5de3ef89d6172b6fdc69e52",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
